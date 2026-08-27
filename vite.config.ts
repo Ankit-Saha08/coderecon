b5f09cc6c716cb6553e88672f06b29d6b80415e3
ANKIT SAHA <85545712+Ankit-Saha08@users.noreply.github.com>
2026-08-27 15:09:59 +0530

    Update vite.config.ts ... v2

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react';
 import path from 'node:path';
 
 export default defineConfig({
+  base: '/coderecon/',
   plugins: [react()],
   resolve: {
     alias: { '@': path.resolve(import.meta.dirname, './src') },
   },
   worker: { format: 'es' },
-  base: './'
 });
